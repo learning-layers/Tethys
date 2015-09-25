9177e71ed1e9f6f8112222565e7f703c4ecb9587
chocoloper <chocoloper@thneed.de>
2015-09-25 16:57:44 +0200

    some documentation

--- src/main/java/de/dbis/acis/cloud/Tethys/util/CORSFilter.java
+++ src/main/java/de/dbis/acis/cloud/Tethys/util/CORSFilter.java
@@ -1,7 +1,5 @@
 package de.dbis.acis.cloud.Tethys.util;
 
-
-
 import java.io.IOException;
 
 import javax.servlet.*;
@@ -9,55 +7,44 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * This class provides additional Headers to each response made by Tethys to enable CORS (Cross Origin Resource Sharing).
+ *
+ * @author Gordon Lawrenz <lawrenz@dbis.rwth-aachen.de>
+ */
 public class CORSFilter implements Filter {
-	
-    public void destroy() {
-    }
-    public static String VALID_METHODS = "DELETE, HEAD, GET, OPTIONS, POST, PUT";
 
-//    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-//        HttpServletRequest httpReq = (HttpServletRequest) req;
-//        HttpServletResponse httpResp = (HttpServletResponse) resp;
-//
-//        // No Origin header present means this is not a cross-domain request
-//        String origin = httpReq.getHeader("Origin");
-//         if (origin == null) {
-//            // Return standard response if OPTIONS request w/o Origin header
-//           if ("OPTIONS".equalsIgnoreCase(httpReq.getMethod())) {
-//                httpResp.setHeader("Allow", VALID_METHODS);
-//                httpResp.setStatus(200);
-//                return;
-//            }
-//        } else {
-//            // This is a cross-domain request, add headers allowing access
-//            httpResp.setHeader("Access-Control-Allow-Origin", origin);
-//            httpResp.setHeader("Access-Control-Allow-Methods", VALID_METHODS);
-//
-//            String headers = httpReq.getHeader("Access-Control-Request-Headers");
-//            if (headers != null)
-//                httpResp.setHeader("Access-Control-Allow-Headers", headers);
-//
-//            // Allow caching cross-domain permission
-//            httpResp.setHeader("Access-Control-Max-Age", "3600");
-//        }
-//        // Pass request down the chain, except for OPTIONS
-//        if (!"OPTIONS".equalsIgnoreCase(httpReq.getMethod())) {
-//            chain.doFilter(req, resp);
-//        }
-// }
-	public void doFilter(
-			ServletRequest request, ServletResponse response, 
-			FilterChain chain) throws IOException, ServletException {
+  /**
+   * Contains all allowed HTTP methods
+   */
+  public static String VALID_METHODS = "DELETE, HEAD, GET, OPTIONS, POST, PUT";
 
-			((HttpServletResponse)response).addHeader("Access-Control-Allow-Origin", "*");
-			((HttpServletResponse)response).addHeader("Access-Control-Allow-Methods","GET, POST, DELETE, PUT");
-			((HttpServletResponse)response).addHeader("Access-Control-Allow-Headers","Accept, Content-Type, Origin, X-Auth-Token");
+  /**
+   * Adds the Headers
+   */
+  public void doFilter(
+		ServletRequest request, ServletResponse response,
+		FilterChain chain) throws IOException, ServletException {
 
-			chain.doFilter(request, response);
-		}
+		((HttpServletResponse)response).addHeader("Access-Control-Allow-Origin", "*");
+		((HttpServletResponse)response).addHeader("Access-Control-Allow-Methods", VALID_METHODS);
+		((HttpServletResponse)response).addHeader("Access-Control-Allow-Headers", "Accept, Content-Type, Origin, X-Auth-Token");
 
-    public void init(FilterConfig config) throws ServletException {
+		chain.doFilter(request, response);
+	}
 
-    }
-	
+  /**
+   * Empty but needed
+   */
+  public void init(FilterConfig config) throws ServletException {
+    // nothing to do here
+  }
+
+  /**
+   * Empty but needed
+   */
+  public void destroy() {
+    // nothing to do here
+  }
+
 }
